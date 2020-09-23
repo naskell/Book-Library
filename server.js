37388d446b6516dbe9def4b5c7997a45ca0fa9f8
Nasar Khan <nasar.n.khan1@gmail.com>
2020-09-23 11:09:48 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')  // ./ - relative to where we are. Set the indexRouter to router in index.js
+const authorRouter = require('./routes/authors') 
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout') // Every file is put in layout, so that we don't have to duplicate all HTML such as header and footer
+app.set('layout', 'layouts/layout') // This means we don't have to duplicate all HTML such as header and footer
 app.use(expressLayouts)
-app.unsubscribe(express.static('public'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,6 +27,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter) // tell our app to use our indexRouter
+app.use('/authors', authorRouter) // every route inside of authorRouter will be prepended with '/authors'
 
 app.listen(process.env.PORT || 3000)
 
